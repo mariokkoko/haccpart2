194c374991666eca251ba75a790d734add8757c5
mariokkoko <marikoy2025@gmail.com>
2023-12-04 14:28:22 -1000

    fixed page2 link -Mia

--- src/pages/page2.tsx
+++ src/pages/page2.tsx
@@ -6,7 +6,7 @@ export default function page2() {
         <h1>wahhooo</h1>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             
-          <Link href="http://localhost:3000/">Back to home</Link>
+          <Link href="/">Back to home</Link>
         </button>
       </>
     );
