88d773693e7eaf01c9ad768bc0bd5bc21f10574d
gastropodGroupie <taliabenson07@gmail.com>
2023-12-12 11:44:37 -1000

    added daisyui to tailwind.config.ts

--- src/pages/page2.tsx
+++ src/pages/page2.tsx
@@ -4,7 +4,7 @@ export default function page2() {
     return (
       <>
         <h1>wahhooo</h1>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="btn btn-primary">
             
           <Link href="/">Back to home</Link>
         </button>
