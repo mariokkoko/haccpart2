949df6ff2a1512a488a99763810c429849eaae4e
mariokkoko <marikoy2025@gmail.com>
2024-01-17 11:31:32 -1000

    Edited page3.tsx, created forum.tsx

--- src/pages/page3.tsx
+++ src/pages/page3.tsx
@@ -6,13 +6,48 @@ export default function LandingPage() {
   return (
     <>
       <Head>
-        <title>Page One</title>
+        <title>Discover Page</title>
         <meta name="description" content="This is the first page." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <div>
-          <Link className="underline text-blue-600" href="/">ur mom this is a link</Link>
+          <div className="navbar bg-green-900">
+            <Link className="btn byn-ghost text-xl" href="page1">Home</Link>
+            <Link className="btn byn-ghost text-xl" href="page2">Upload Photo</Link>
+            <Link className="btn byn-ghost text-xl" href="postclick">Post Click Page</Link>
+            <Link className="btn byn-ghost text-xl" href="forum">Forum</Link>
+          </div>
+          <div className="pt-4">
+            <h1 className="text-center text-5xl">Discover Page</h1>
+          </div>
+          <div className="pt-4">
+            <input type="text-center" placeholder="Search Bar" className="input input -bordered w-full max-w-xd text-center text-xl"></input>
+          </div>
+          <table className="flex table-fixed justify-center items-center">
+            <tbody>
+            <tr>
+              <td>
+                <figure className="max-w-lg">
+                  <img className="rounded-lg" src="https://wpcdn.us-east-1.vip.tn-cloud.net/www.hawaiimagazine.com/content/uploads/2020/04/p/n/ohialehua-gettyimages-622449416-1024x683.jpg" alt="image description"></img>
+                  <figcaption className="mt-2 text-2xl text-center text-black-500 dark:text-black-400">'Ohi'a Lehua</figcaption>
+                </figure>
+              </td>
+              <td>
+                <figure className="max-w-lg">
+                  <img className="rounded-lg" src="https://merwinconservancy.org/wp-content/uploads/2016/05/Pritchardia-Hillebrandii-a-native-Hawaiian-palm-SM-1024x682.jpg" alt="image description"></img>
+                  <figcaption className="mt-2 text-2xl text-center text-black-500 dark:text-black-400">Loulu Palm</figcaption>
+                </figure>
+              </td>
+              <td>
+                <figure className="max-w-lg">
+                  <img className="rounded-lg" src="https://irp-cdn.multiscreensite.com/5be3da7c/IMG_4986.jpg" alt="image description"></img>
+                  <figcaption className="mt-2 text-2xl text-center text-black-500 dark:text-black-400">Naupaka</figcaption>
+                </figure>
+              </td>
+            </tr>
+            </tbody>
+          </table>
         </div>
       </main>
     </>
