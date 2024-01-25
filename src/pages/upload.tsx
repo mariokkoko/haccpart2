bfb78c0e178a6dadad870a3840d3847b5502737e
Mia131313 <mhamamoto83@gmail.com>
2024-01-25 13:33:34 -1000

    Mia 1/25

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -3,11 +3,15 @@ import Link from 'next/link';
 export default function page2() {
     return (
       <>
+      <div>
         <h1>dis da upload page</h1>
         <button className="btn btn-primary">
             
           <Link href="/">Back to home</Link>
         </button>
+
+        <input type="file" className="file-input file-input-bordered file-input-success w-full max-w-xs" />
+        </div>
       </>
     );
   }
